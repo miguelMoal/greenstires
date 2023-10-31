71f3c5b13896d57f0e95af7521980b807bce9493
miguel522react <53583059+miguel522react@users.noreply.github.com>
2023-10-31 10:00:51 -0600

    first

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function App() {
+  return (
+    <div>
+      <div class="mx-auto max-w-[1440px] ">
+        <p>kjbsdfjkdbsfkjdbskjb</p>
+      </div>
+    </div>
+  );
+}
